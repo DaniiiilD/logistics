bf27dc8c1978bc2a20e6e30a9a69934b2bed9e20
DaniiiilD <daniil_code@example.com>
2026-04-27 16:31:49 +0300

    OrderOffer table by alembic

--- src/orm/models/__init__.py
+++ src/orm/models/__init__.py
@@ -1,10 +1,9 @@
 from src.orm.database import Base
-
 from src.orm.models.user import User
 from src.orm.models.company.company import Company
 from src.orm.models.driver.driver import Driver
 from src.orm.models.company.order import Order
 from src.orm.models.driver.offer import OrderOffer
-from orm.models.vehicle.vehicle import Vehicle
+from src.orm.models.vehicle.vehicle import Vehicle
 
 __all__ = ["Base", "User", "Company", "Order", "Driver", "OrderOffer", "Vehicle"]
